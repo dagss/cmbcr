89071526c35797b221b1ca17a5bd4d7d1e4ce899
Dag Sverre Seljebotn <d.s.seljebotn@astro.uio.no>
2017-08-25 12:45:44 +0200

    wip

--- scripts/sinv_fft.py
+++ scripts/sinv_fft.py
@@ -23,7 +23,7 @@ from cmbcr.utils import *
 
 from cmbcr import sharp
 from healpy import mollzoom, mollview
-from scipy.sparse import csc_matrix
+from scipy.sparse import csc_matrix, dok_matrix
 #reload(cmbcr.main)
 
 from cmbcr.cr_system import load_map_cached
@@ -37,8 +37,8 @@ lmax = 256 - 1
 lmax_sh = int(1 * lmax)
 OMEGA = 1
 
-SQRTSPLITFULLSOLVE = False
-
+SQRTSPLITFULLSOLVE = True
+INNER_ITS = 1
 
 def load_Cl_cmb(lmax, filename='camb_11229992_scalcls.dat'):
     #dat = np.loadtxt()
@@ -54,10 +54,10 @@ Cl_cmb = load_Cl_cmb(10000)
 
 l = np.arange(1, lmax_sh + 2).astype(float)
 #dl = np.exp(0.01 * l)
-dl = l**3
-#dl = 1 / Cl_cmb[::2][:lmax_sh + 1]
+dl = l**4
+#dl = 1 / Cl_cmb[::16][:lmax_sh + 1]
 
-nl = cmbcr.standard_needlet_by_l(3, 2 * dl.shape[0] - 1)
+nl = cmbcr.standard_needlet_by_l(2, 2 * dl.shape[0] - 1)
 i = nl.argmax()
 dl = np.concatenate([dl, nl[i:] * dl[-1] / nl[i]])
 
@@ -114,112 +114,6 @@ def flatsky_synthesis(u):
 
 def flatsky_adjoint_synthesis(u):
     return np.fft.fftn(u)
-
-
-class BlockSmoother(object):
-    def __init__(self, level):
-        self.level = level
-        self.tilesize = 1
-
-        # hammer operator on a tilesize x tilesize patch...
-        u = np.zeros((level.nrows, level.ncols))
-        block = np.zeros((self.tilesize**2, self.tilesize**2))
-        for i in range(self.tilesize):
-            for j in range(self.tilesize):
-                idx = i * self.tilesize + j
-                u[:, :] = 0
-                u[i, j] = 1
-                u = flatsky_adjoint_synthesis(u)
-                u *= np.sqrt(level.dl_fft)
-                u = flatsky_synthesis(u).real
-
-                x = u.reshape((level.nrows // self.tilesize, self.tilesize, level.ncols // self.tilesize, self.tilesize))
-                x = x[0, :, 0, :].reshape(self.tilesize**2)
-                
-                block[:, idx] = x
-        self.block_inv = np.linalg.inv(block)
-        
-
-    def apply(self, u):
-        u = self.level.padvec(u)
-
-        # pad u_in in order to wrap around
-        tmp = np.hstack([u, u, u])
-        u_in = np.vstack([tmp, tmp, tmp])
-
-        u_out = np.zeros_like(u_in)
-
-        overlap = 0
-        
-        k = self.tilesize
-        n, m = self.level.nrows, self.level.ncols
-        for i in range(0, n + k - overlap, k):
-            for j in range(0, m + k - 1, k):
-                s = (
-                    slice(n + i - overlap, n + i + k - overlap),
-                    slice(m + j - overlap, m + j + k - overlap)
-                    )
-                tile = u_in[s]
-                assert tile.shape == (k, k)
-                tile = np.dot(self.block_inv, tile.reshape(k**2)).reshape(k, k)
-                
-                # downweight the edges of the tile, which overlaps with other tiles
-                if overlap:
-                    tile[:, :] *= 0.5
-                    tile[overlap:-overlap, overlap:-overlap] *= 2
-                
-                u_out[s] += tile
-
-        u_out = u_out[n:2 * n, m:2 * m]
-        u = OMEGA * self.level.pickvec(u_out)
-        return u
-    
-        
-from scipy.sparse import dok_matrix
-
-def coarsen_matrix(coarse_level, fine_level):
-    R = dok_matrix((coarse_level.n_mask, fine_level.n_mask))
-
-    def add(coarse_i, coarse_j, fine_i, fine_j, weight):
-        # wrap around fine_i and fine_j..
-        fine_i = fine_i % fine_level.nrows
-        fine_j = fine_j % fine_level.ncols
-
-        coarse_ind = coarse_level.ind_map[coarse_i, coarse_j]
-        fine_ind = fine_level.ind_map[fine_i, fine_j]
-        if coarse_ind != -1 and fine_ind != -1:
-            R[coarse_ind, fine_ind] = weight
-    
-    for i in range(coarse_level.nrows):
-        for j in range(coarse_level.ncols):
-            if 0:
-                # row above
-                add(i, j, 2 * i - 1, 2 * j - 1, 1/16.)
-                add(i, j, 2 * i - 1, 2 * j, 1/8.)
-                add(i, j, 2 * i - 1, 2 * j + 1, 1/16.)
-
-                # center row
-                add(i, j, 2 * i, 2 * j - 1, 1/8.)
-                add(i, j, 2 * i, 2 * j, 1/4.)
-                add(i, j, 2 * i, 2 * j + 1, 1/8.)
-
-                # row below
-                add(i, j, 2 * i + 1, 2 * j - 1, 1/16.)
-                add(i, j, 2 * i + 1, 2 * j, 1/8.)
-                add(i, j, 2 * i + 1, 2 * j + 1, 1/16.)
-            else:
-                # row above
-                add(i, j, 2 * i - 1, 2 * j, 1/8.)
-
-                # center row
-                add(i, j, 2 * i, 2 * j - 1, 1/8.)
-                add(i, j, 2 * i, 2 * j, 1/2.)
-                add(i, j, 2 * i, 2 * j + 1, 1/8.)
-
-                # row below
-                add(i, j, 2 * i + 1, 2 * j, 1/8.)
-
-    return R.tocsr()
 
 
 def full_coarsen_matrix(ntheta, nphi):
@@ -285,7 +179,12 @@ u = sharp.sh_synthesis_gauss(lmax, u, lmax_sh=lmax_sh).reshape((nrings, 2 * nrin
 image_of_operator = u[start_ring:stop_ring, :]
 
 
-dl_fft = image_to_powspec(unitvec, image_of_operator)
+outer_dl_fft = image_to_powspec(unitvec, image_of_operator)
+
+if SQRTSPLITFULLSOLVE:
+    inner_dl_fft = np.sqrt(outer_dl_fft)
+else:
+    inner_dl_fft = outer_dl_fft
 
 
 class Level(object):
@@ -399,7 +298,7 @@ def coarsen_level(level):
     return Level(dl_fft_H, mask_H)
 
 
-cur_level = level = Level(dl_fft, mask)
+cur_level = root_level = Level(inner_dl_fft, mask)
 levels = [cur_level]
 
 while cur_level.n > 500:
@@ -410,186 +309,67 @@ smoothers = [DiagonalSmoother(lev) for lev in levels]
 
 
 
-rng = np.random.RandomState(12)
-x0 = rng.normal(size=level.n)
-
 matvec_count = 0
 
-def precond(b):
+def inner_precond(b):
     x = v_cycle(0, levels, smoothers, b)
     return x
 
-if 1:
-    b = level.matvec(x0)
-
-    solver = cg_generator(
-        level.matvec,
-        b,
-        M=precond
-        )
+inner_matvec = root_level.matvec
 
 
-norm0 = np.linalg.norm(x0)
-errlst = []
+def outer_matvec(u):
+    global matvec_count
+    matvec_count += 1
+    u = root_level.padvec(u)
+    u = flatsky_adjoint_synthesis(u)
+    u *= outer_dl_fft
+    u = flatsky_synthesis(u)
+    u = root_level.pickvec(u).real
+    return u
 
-for i, (x, r, delta_new) in enumerate(solver):
-    errvec = x0 - x
-    errlst.append(np.linalg.norm(errvec) / norm0)
-    if i > 30:
-        break
-
-
-
-#clf();
-#imshow(padvec(x-x0), interpolation='none');
-#semilogy(matvec_counts, errlst, '-o')
-semilogy(errlst, '-o')
-#colorbar()
-draw()
-
-
-
-
-
-if 0:
-    A_h = hammer(level.matvec, level.n_mask)
-    R = coarseners[0].toarray()
-
-    A_H = np.dot(R, np.dot(A_h, R.T))
-    
-    A_H_0 = hammer(levels[1].matvec, levels[1].n_mask)
-
-    clf()
-    plot(A_H.diagonal())
-    plot(A_H_0.diagonal())
-    draw()
-    1/0
-    
-
-
-    
-
-def jacobi(matvec, precond, b, n):
-    x = precond(b)
-    for i in range(n - 1):
-        x += precond(b - matvec(x))
-    return x
-    
-
-    
-if 0:
-
-    A = hammer(level.matvec, level.n_mask)
-    M = hammer(precond, level.n_mask)
-    clf()
-    semilogy(np.linalg.eigvalsh(A))
-    semilogy(np.linalg.eigvalsh(M)[::-1])
-    lam = np.linalg.eig(np.dot(M, A))[0].real
-    semilogy(sorted(lam))
-    print lam.max()
-    #semilogy(np.linalg.eigvalsh(M)[::-1])
-    draw()
-    1/0
-
-
-        
-rng = np.random.RandomState(12)
-x0 = rng.normal(size=level.n_mask)
-
-matvec_count = 0
 
 if SQRTSPLITFULLSOLVE:
-
-    INNERITS = 10
-
-    def matvec(x):
-        global matvec_count
-        matvec_count += 1
-        return level.matvec(x)
     
-    def precond2(b):
-        
-        solver = cg_generator(
-            matvec,
-            b,
-            M=precond
-            )
-
-        for i, (x, r, delta_new) in enumerate(solver):
-            if i > INNERITS:
-                break
-
-        solver = cg_generator(
-            matvec,
-            x,
-            M=precond
-            )
-
-        for i, (x, r, delta_new) in enumerate(solver):
-            if i > INNERITS:
-                break
-            
+    def jacobi(matvec, precond, b, n):
+        x = precond(b)
+        for i in range(n - 1):
+            x += precond(b - matvec(x))
         return x
 
-    def precond2_jac(b):
-        x = jacobi(matvec, precond, b, INNERITS)
-        x = jacobi(matvec, precond, x, INNERITS)
+    def outer_precond(b):
+        x = jacobi(inner_matvec, inner_precond, b, INNER_ITS)
+        #return x
+        x = jacobi(inner_matvec, inner_precond, x, INNER_ITS)
         return x
 
-    def matvec2(u):
-        global matvec_count
-        matvec_count += 1
-        u = level.padvec(u)
-        u = flatsky_adjoint_synthesis(u)
-        u *= level.dl_fft
-        u = flatsky_synthesis(u)
-        u = level.pickvec(u).real
-        return u
-
-    b = matvec2(x0)
-    solver = cg_generator(
-        matvec2,
-        b,
-        M=precond2
-        )
-
-    #A = hammer(matvec2, level.n_mask)
-    #M = hammer(precond2, level.n_mask)
-    #w = np.linalg.eigvals(np.dot(M, A))
-    #clf()
-    #semilogy(sorted(w))
-    #draw()
-    
     
 else:
-    b = level.matvec(x0)
-
-    solver = cg_generator(
-        level.matvec,
-        b,
-        M=precond
-        )
+    outer_precond = inner_precond
 
 
-norm0 = np.linalg.norm(x0)
-errlst = []
+rng = np.random.RandomState(12)
+x0 = rng.normal(size=root_level.n)
+b = outer_matvec(x0)
+
+
+
+solver = cg_generator(
+    outer_matvec,
+    b,
+    M=outer_precond
+    )
+
 matvec_counts = []
-
+errlst = []
+norm0 = np.linalg.norm(x0)
 for i, (x, r, delta_new) in enumerate(solver):
-
-#for i in range(1000):
-#    r = b - matvec_mask_basis(x)
-#    x = x + 0.1 * precond(r)
-
     errvec = x0 - x
-    #err_its.append(errvec)
-    #x_its.append(x)
-
     matvec_counts.append(matvec_count)
     errlst.append(np.linalg.norm(errvec) / norm0)
 
     print 'it', i
-    if i > 5:
+    if i > 20:
         break
 
 
